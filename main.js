6f9144ab295319b16ab37db5a09a22b8b94fd5c1
Alyssha Lewin <alyssha.lew@gmail.com>
2022-06-02 10:53:24 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//button 
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const alienName = document.querySelector('input').value
+    try {
+        const response = await fetch(`https://star-trek-api-demo-1.herokuapp.com/api/${alienName}`)
+        const data = await response.json()
+        console.log(data)
+    } catch(error) {
+        console.log(error)
+    }
+}
